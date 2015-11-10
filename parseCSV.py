0a0cf8242b4b00a6ffe0c150a20f8bca3061862c
Adithya Seshadri <aseshad@ncsu.edu>
2015-11-09 23:57:08 -0500

    Added basic parsing of data.
    Added a python file to parse the raw data into csv.
    Added the parsed CSV

---
+++ parseCSV.py
@@ -0,0 +1,35 @@
+#parseCSV.py
+
+import csv
+import re
+
+f = open("output.txt","r")
+linesplit = []
+for line in f:
+	lsp = re.split('\(|\)|\n',line)
+	for sp in lsp:
+		if(sp=='.' or sp=='' or sp==' '):
+			lsp.remove(sp)
+	linesplit.append(lsp)
+
+r = re.compile('(.{2}|.|):.{2}')
+
+
+with open('NFLparse.csv','wb') as fp:
+	writer = csv.writer(fp,delimiter=',')
+	writer.writerow(["Game state","Game Time","Formation","Play information",])
+	for line in linesplit:
+		if len(line) > 1:
+			if not r.match(line[1]):
+				line.insert(1, " ")
+		if len(line) > 2:
+			if not ('Shotgun' in line[2] or 'Field Goal formation' in line[2] or 'Punt formation' in line[2]):
+				line.insert(2," ")
+		if len(line[-1]) == 0:
+			line = line[:-1]
+		if len(line) > 3:
+			line[3:] = [';'.join(line[3:])]
+
+		print line, len(line)
+		writer.writerow(line)
+
