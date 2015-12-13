721a28e3c438bdf4f01767222a02d93eedd16173
Adithya Seshadri <aadithya93@gmail.com>
2015-12-13 10:37:04 -0500

    updated markov analysis

--- markovAnalysis.py
+++ markovAnalysis.py
@@ -19,6 +19,15 @@ def timeToSeconds(s):
 	return minute * 60 + int(tup[1])
 
 
-nfl = pd.read_csv("playByPlay.csv",header=0)
+nfl = pd.read_csv("playByPlayDataset.csv",header=0)
 nfl['quarterTime'] = nfl['quarterTime'].map(timeToSeconds)
-nflData = nfl[['year', 'week', 'possession', 'yardsToGoalLine', 'quarter', 'down', 'togo', 'quarterTime', 'shotgun', 'complete', 'distance', 'direction', 'yardsGained', 'intercepted', 'noHuddle', 'touchdown', 'fumble', 'sacked', 'spiked', 'runDirection']]
+nflData = nfl[['year', 'week', 'possession', 'yardsToGoalLine', 'quarter', 'down', 'togo', 'quarterTime', 'shotgun', 'complete', 'distance', 'direction', 'yardsGained', 'intercepted', 'noHuddle', 'touchdown', 'fumble', 'sacked', 'spiked', 'runDirection','playType']]
+nflData.week = pd.Series(nflData.week, dtype="category")
+nflData.possession = pd.Series(nflData.possession, dtype="category")
+nflData.quarter = pd.Series(nflData.quarter, dtype="category")
+nflData.down = pd.Series(nflData.down, dtype="category")
+#nflData.complete = pd.Series(nflData.complete, dtype="bool")
+nflData.distance = pd.Series(nflData.distance, dtype="category")
+nflData.direction = pd.Series(nflData.direction, dtype="category")
+nflData.runDirection = pd.Series(nflData.runDirection, dtype="category")
+nflData.playType = pd.Series(nflData.playType, dtype="category")
