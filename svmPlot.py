47ac16ccb0715a0a6aa3fbbc0d3a1efb6afc80c7
Adithya Seshadri <aadithya93@gmail.com>
2015-12-14 20:41:59 -0500

    updated svm plots

--- svmPlot.py
+++ svmPlot.py
@@ -14,7 +14,9 @@ data={}
 for f in files:
     year = f.split('.')[0]
     data[year] = pd.read_csv('svmResults/'+f,header=0)
-    data[year].plot(x=data[year].Team,kind='bar',figsize=(14,10),title="Run vs. Pass Prediction Accuracy "+year)
+    del data[year]['TrainAccuracy']
+    ax = data[year].plot(x=data[year].Team,kind='bar',figsize=(14,10),title="Run vs. Pass Prediction Accuracy "+year)
+    ax.set_ylim([min(min(data[year].TestAccuracy), min(data[year].PlayDistribution)) * 0.97, max(max(data[year].TestAccuracy), max(data[year].PlayDistribution)) * 1.03])
     plt.savefig('svmResults/Accuracy'+year+'.png')
     
 
@@ -36,14 +38,15 @@ for team in teams:
 meanData = pd.DataFrame({'Team':teams})
 meanData['MeanTestAccuracies'] = MeanTestAcc
 meanData['MeanPlayDistribution'] = MeanPlayDist
-meanData.plot(x = meanData.Team,kind='bar',figsize=(14,10),title="Mean Run vs. Pass Accuracy")
+ax1 = meanData.plot(x = meanData.Team,kind='bar',figsize=(14,10),title="Mean Run vs. Pass Accuracy")
+ax1.set_ylim([min(min(meanData.MeanTestAccuracies), min(meanData.MeanPlayDistribution)) * 0.97, max(max(meanData.MeanTestAccuracies), max(meanData.MeanPlayDistribution)) * 1.03])
 plt.savefig('svmResults/MeanComparison.png') 
 
 score = []
 for i in range(len(teams)):
-    score.append(MeanTestAcc[i] - MeanPlayDist[i])
+    score.append((MeanTestAcc[i] - MeanPlayDist[i])*100)
     
 scoreData = pd.DataFrame({'Team':teams})
 scoreData['Score'] = score
-scoreData.plot(kind='bar',x='Team',y='Score',figsize=(14,10))
+ax2 = scoreData.plot(kind='bar',x='Team',y='Score',figsize=(14,10))
 plt.savefig('svmResults/ScoreSVM.png')     
